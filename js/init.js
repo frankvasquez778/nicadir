2ed132d62b07fed6bb8321f222401dc8f2d92399
frankvasquez778 <frankvasquez778@gmail.com>
2024-12-09 16:51:19 -0600

    Delete js directory

--- js/init.js
+++
@@ -1,7 +0,0 @@
-
-
-// js/init.js
-
-// Inicializar Parse con las claves correctas
-Parse.initialize("qWRUPwIAHwxC6SlBujz7VNVk1GgHAzUr5pIVq9bw", "CzjbhomeSRolrQmalCYF7KsSCugHCyQrsr4jRVnf");
-Parse.serverURL = 'https://parseapi.back4app.com/';
