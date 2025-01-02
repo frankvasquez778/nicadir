e785026b43b9ec3481c73b75fc50fdbddb3b07c3
frankvasquez778 <frankvasquez778@gmail.com>
2025-01-02 10:12:00 -0600

    Delete control directory

--- control/js/parseConfig.js
+++
@@ -1,6 +0,0 @@
-// scripts/parseConfig.js
-// Configuración inicial de Parse
-Parse.initialize("bnNvtaeeY4jugtHymmQXuppvhhkYFVHh1LrLbutl", "aIcriN0MuRypqNoyLo6h6kPRuAUXK5tIr6Ijdq16"); // Reemplaza con tus credenciales de Back4App
-Parse.serverURL = "https://parseapi.back4app.com/";
-
-export default Parse;
